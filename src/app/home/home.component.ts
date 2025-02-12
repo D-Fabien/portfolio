f29c94c64a52186bfd9594ffe4006ecacc399658
D-Fabien <fabien.delahogue@gmail.com>
2025-02-12 11:23:50 +0100

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+}
+
+
